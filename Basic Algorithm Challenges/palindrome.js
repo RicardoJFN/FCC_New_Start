4f1e7de542d99f4288ec0571df5bb877b4ba525b
ricardojfn <ricardo.nunes.isla@gmail.com>
2017-07-27 18:08:51 +0100

    Added 2 algorithms that are properly working. Working on palindrome one.

---
+++ Basic Algorithm Challenges/palindrome.js
@@ -0,0 +1,42 @@
+/*
+Return true if the given string is a palindrome. Otherwise, return false.
+
+A palindrome is a word or sentence that's spelled the same way both forward and backward, 
+ignoring punctuation, case, and spacing.
+
+Note
+You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols)
+ and turn everything lower case in order to check for palindromes.
+
+We'll pass strings with varying formats, 
+such as "racecar", "RaceCar", and "race CAR" among others.
+
+We'll also pass strings with special symbols, such as "2A3*3a2", "2A3  3a2", and "2_A3*3#A2".
+
+Remember to use Read-Search-Ask if you get stuck. Write your own code.
+
+Here are some helpful links:
+
+    String.prototype.replace()
+
+    String.prototype.toLowerCase()
+
+
+    [^a-zA-Z\d\s:]
+*/
+
+function replacer(match, p3, offset, string) {
+    return [p3].join('');
+}
+
+var newString = "A man, a plan, a canal. Panama".replace(/([^a-zA-Z\d\s:])/);
+
+function palindrome(str) {
+    
+
+    
+    return str;
+}
+
+//console.log(palindrome("eye"));
+console.log(newString);
