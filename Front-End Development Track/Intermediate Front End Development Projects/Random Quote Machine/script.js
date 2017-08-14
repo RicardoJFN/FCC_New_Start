cac8d373c820ae788a9f05837df1c4db1ed9f6c7
ricardojfn <ricardo.nunes.isla@gmail.com>
2017-08-14 22:12:25 +0100

    Added the code for a simple layout in html file and css. In JavaScript file, added the correct code to get the API do work. You need to use a plugin in the browser to permit the CORS, otherwise it won't work

--- Front-End Development Track/Intermediate Front End Development Projects/Random Quote Machine/script.js
+++ Front-End Development Track/Intermediate Front End Development Projects/Random Quote Machine/script.js
@@ -1,9 +1,17 @@
 var text = "";
 
-$document.ready(function() {
-    text += $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=", function(a) {
-        $("body").append(a[0].content + "<p>— " + a[0].title + "</p>")
-    });
-});
 
-console.log(text);
+$(document).ready(function() {
+    $("#button").click(function() {
+        text += $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1", function(a) {
+            //$("#center_slot").append(a[0].content + "<p> " + a[0].title + "</p>")
+            //$("#title").text(a[0].title);
+            //$("#content").text(a[0].content);
+            $("#center_slot").find('#title').text(a[0].title);
+            $("#center_slot").find('#content').text(a[0].content);
+        });
+    });
+    //Setting the cache false, this way it gives a new random quote
+    $.ajaxSetup({cache: false});
+    
+});
